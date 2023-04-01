eb18ad3e1d5f55790b479888c174839b88a50ced
Misha <mishalxv35@gmail.com>
2023-04-01 17:13:45 -0400

    moving to windows

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,25 +10,22 @@ const app = express();
 
 app.use(express.json());
 
+// Routers
+app.use(`${BASE_URL}/users`, userRouter);
 
-
-// Routers 
-app.use(`${BASE_URL}/users`,userRouter);
-
-
-const PORT = process.env.SERVER_PORT || 8000;
+const PORT = Number(process.env.SERVER_PORT)  || 8000;
 
 const start = async (): Promise<void> => {
-    try{
-        sequelize.sync({force: false, logging: false}).then(() => {
-            app.listen(PORT, () => {
-                console.log(`Server is running on http://localhost:${PORT}`);
-            });
-        });
-    }catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+  try {
+    sequelize.sync({ force: false, logging: false }).then(() => {
+      app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
-start(); 
+start();
