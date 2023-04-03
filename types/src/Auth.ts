9711b2975ba000ee929d4b478efe198a6b9b82f3
Misha Alexeev <mishalxv35@gmail.com>
2023-04-02 20:11:19 -0400

    reworked some models and added seeding scripts

--- types/src/Auth.ts
+++ types/src/Auth.ts
@@ -1,5 +1,24 @@
 export interface ValidationErrorSimple {
-  field: string,
-  errors: {[type: string]: string}
+  field: string;
+  errors: { [type: string]: string };
 }
 
+export interface AuthErrors {
+  user_errors?: ValidationErrorSimple[];
+  stats_errors?: ValidationErrorSimple[];
+}
+
+export interface Login {
+  token: string;
+}
+
+export interface Register {
+  message: string;
+}
+
+export interface ApiError {
+  message: string;
+}
+
+export type LoginResponse = Login | AuthErrors | ApiError;
+export type RegisterResponse = Register | AuthErrors | ApiError;
