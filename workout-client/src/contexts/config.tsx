63d7180d937f304b18b37ee2efc1264fdf6d0e35
Mikhail Alexeev <mishalxv35@gmail.com>
2023-06-20 13:14:22 -0700

    config context

---
+++ workout-client/src/contexts/config.tsx
@@ -0,0 +1,66 @@
+import React, {
+  createContext,
+  FC,
+  ReactNode,
+  useEffect,
+  useState,
+  useContext,
+} from "react";
+import * as SecureStore from "expo-secure-store";
+import { LanguageType, UnitType } from "../types/config";
+
+type ConfigType = {
+  units: UnitType;
+  language: LanguageType;
+};
+
+export interface IConfigContextInterface {
+	config: ConfigType;
+  updateUnits: (unit: UnitType) => void;
+  updateLanguage: (language: LanguageType) => void;
+}
+
+interface ConfigProps {
+  children: ReactNode;
+}
+
+const ConfigContext = createContext<IConfigContextInterface | null>(null);
+
+const ConfigProvider: FC<ConfigProps> = ({ children }) => {
+	const [config, setConfig] = useState<ConfigType>({units: "metric", language: "en"});
+
+
+  const getSavedConfig = async () => {
+    const configJson = await SecureStore.getItemAsync("config");
+    let config = {};
+    if (configJson){
+        config = JSON.parse(configJson);
+    }
+		setConfig(config as ConfigType);		
+  };
+
+  useEffect(() => {
+    getSavedConfig();
+  });
+
+	useEffect(() => {
+		// save updated config to local storage
+		console.log(config);
+		SecureStore.setItemAsync("config", JSON.stringify(config));
+	}, [config]);
+
+	const updateLanguage = (language: LanguageType) => {
+		setConfig({...config, language: language});
+	} 
+	
+	const updateUnits = (units: UnitType) => {
+		setConfig({...config, units: units});
+	}
+
+	return(
+		<ConfigContext.Provider value={{config, updateUnits, updateLanguage}}>
+			{children}
+		</ConfigContext.Provider>
+	)
+
+};
