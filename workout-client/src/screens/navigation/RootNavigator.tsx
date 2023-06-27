eedb62c6b16bc9f9d29f3725c5047399286d6101
Mikhail Alexeev <mishalxv35@gmail.com>
2023-06-26 22:33:12 -0700

    removed font load todo for now

--- workout-client/src/screens/navigation/RootNavigator.tsx
+++ workout-client/src/screens/navigation/RootNavigator.tsx
@@ -4,13 +4,13 @@ import { RootStackParamList } from "../../types/navigation";
 import TabNavigator from "./TabNavigator";
 import SplashScreen from "../SplashScreen";
 import AuthNavigator from "./AuthNavigator";
+import {useFonts} from "expo-font";
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
   const { authState } = useAuth();
 
-  //TODO: Add font loading check
   if (authState?.isRehydrating) {
     return <SplashScreen/>;
   }
