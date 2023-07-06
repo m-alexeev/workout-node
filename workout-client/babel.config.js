8447485fc263ada0656b37bef889507cd306bd49
Misha Alexeev <mishalxv35@gmail.com>
2023-07-05 20:50:36 -0400

    firebase .env

--- workout-client/babel.config.js
+++ workout-client/babel.config.js
@@ -6,6 +6,9 @@ module.exports = function(api) {
       production: {
         plugins: ['react-native-paper/babel'],
       }
-    }
+    },
+    plugins: [
+      ["module:react-native-dotenv"]
+    ]
   };
 };
--- workout-client/firebase.js
+++ workout-client/firebase.js
@@ -1,19 +1,32 @@
-import { initializeApp, getFirestore } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth, initializeAuth, reactNativeLocalPersistence } from "firebase/auth";
+
+import {
+  FIREBASE_DOMAIN,
+  FIREBASE_DB_URL,
+  FIREBASE_PROJ_ID,
+  FIREBASE_MEASURE_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MESSAGING_ID,
+  FIREBASE_API_KEY,
+  FIREBASE_STORAGE_BUCKET
+} from "@env";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZu6LgcoZQTeH-bfcWwSYnfnldwq1_ueU",
-  authDomain: "workout-2531c.firebaseapp.com",
-  databaseURL: "https://workout-2531c-default-rtdb.firebaseio.com",
-  projectId: "workout-2531c",
-  storageBucket: "workout-2531c.appspot.com",
-  messagingSenderId: "755490275970",
-  appId: "1:755490275970:web:169571cca7fe2f6b70aa9b",
-  measurementId: "G-RNXY5VSQ1W",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_DOMAIN,
+  databaseURL:FIREBASE_DB_URL,
+  projectId: FIREBASE_PROJ_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASURE_ID,
 };
 
 // Initialize Firebase
@@ -22,8 +35,6 @@ const app = initializeApp(firebaseConfig);
 // keep data persistence between reloads
 initializeAuth(app, { persistence: reactNativeLocalPersistence });
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-// const db = getFirestore(app);
-export {app, auth}
-
-// export {app, db, auth};
+export { app, db, auth };
